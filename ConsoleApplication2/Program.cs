e74484ba57a50afbd194e12155ca456b4dfa2e1a
nbadro <37700181+nbadro@users.noreply.github.com>
2018-06-25 19:19:40 +0200

    Update Program.cs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World")
         }
     }
 }
